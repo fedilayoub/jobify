4120f204af0a443846b94a99c5d473020b3354fc
fedilayoub <ayoubzahi2016@gmail.com>
2023-08-25 11:20:17 +0100

    separated routers and controllers from server

---
+++ controllers/jobController.js
@@ -0,0 +1,62 @@
+import { nanoid } from "nanoid";
+let jobs = [
+  {
+    id: nanoid(),
+    company: "Google",
+    position: "Software Engineer",
+  },
+  {
+    id: nanoid(),
+    company: "Facebook",
+    position: "Software Engineer",
+  },
+];
+
+export const getAllJobs = async (req, res) => {
+  res.status(200).send(jobs);
+};
+
+export const createJob = async (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).send("Company and Position are required");
+  }
+  const job = { id: nanoid(), company, position };
+  jobs.push(job);
+  res.status(201).send({ job });
+};
+
+export const getJob = async (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).send(`No job with id ${id} was found`);
+  }
+  res.status(200).send({ job });
+};
+
+export const editJob = async (req, res) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).send("Company and Position are required");
+  }
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).send(`No job with id ${id} was found`);
+  }
+  job.company = company;
+  job.position = position;
+  res.status(200).send({ job });
+};
+
+export const deleteJob = async (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).send(`No job with id ${id} was found`);
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+  res.status(200).send({ msg: `Job with id ${id} was deleted` });
+};
--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
-import { nanoid } from "nanoid";
+import jobRouter from "./routes/jobRouter.js";
 
 dotenv.config();
 const app = express();
 
-const jobs = [
-  {
-    id: nanoid(),
-    company: "Google",
-    position: "Software Engineer",
-  },
-  {
-    id: nanoid(),
-    company: "Facebook",
-    position: "Software Engineer",
-  },
-];
+
 
 //HTTP request logger middleware for node.js
 if (process.env.NODE_ENV === "development") {
@@ -35,59 +24,8 @@ app.post("/", (req, res) => {
   console.log(req.body);
 });
 
-// GET ALL JOBS
-app.get("/api/v1/jobs", (req, res) => {
-  res.status(200).send(jobs);
-});
-
-// CREATE A JOB
-app.post("/api/v1/jobs", (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).send("Company and Position are required");
-  }
-  const job = { id: nanoid(), company, position };
-  jobs.push(job);
-  res.status(201).send({ job });
-});
-
-//GET A SINGLE JOB
-app.get("/api/v1/jobs/:id", (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).send(`No job with id ${id} was found`);
-  }
-  res.status(200).send({ job });
-});
-
-//EDIT A JOB
-app.patch("/api/v1/jobs/:id", (req, res) => {
-  const { id } = req.params;
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).send("Company and Position are required");
-  }
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).send(`No job with id ${id} was found`);
-  }
-  job.company = company;
-  job.position = position;
-  res.status(200).send({ job });
-});
-
-// DELETE A JOB
-app.delete("/api/v1/jobs/:id", (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).send(`No job with id ${id} was found`);
-  }
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
-  res.status(204).send({ jobs });
-});
+// Job router
+app.use("/api/v1/jobs", jobRouter);
 const port = process.env.PORT || 5100;
 
 app.use("*", (req, res) => {
