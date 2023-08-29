6d93a4c3660be2888803ee40a2b82ac4d2c02ed4
fedilayoub <ayoubzahi2016@gmail.com>
2023-08-29 17:29:21 +0100

    moved job checks to validation middleware

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,30 +1,45 @@
 import { body, param, validationResult } from "express-validator";
-import { BadRequestError } from "../errors/customErrors.js";
+import { BadRequestError, NotFoundError } from "../errors/customErrors.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
+import mongoose from "mongoose";
+import Job from "../models/JobModel.js";
 
 const withValidationErrors = (validateValues) => {
-    return [validateValues, (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          const errorMessages = errors.array().map((err) => err.msg);
-          throw new BadRequestError(errorMessages);
+  return [
+    validateValues,
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map((err) => err.msg);
+        if(errorMessages[0].startsWith("No job")){
+          throw new NotFoundError(errorMessages);
         }
-        next();
-      }]
-}
+       else throw new BadRequestError(errorMessages);
+      }
+      next();
+    },
+  ];
+};
 
-export const validateJobInput = withValidationErrors(
-    [
-      body("company").notEmpty().withMessage("Company is required"),
-      body("position").notEmpty().withMessage("Position is required"),
-      body("jobLocation").notEmpty().withMessage("Job location is required"),
-      body("jobStatus").isIn(Object.values(JOB_STATUS)).withMessage("Invalid job status"),
-      body("jobType").isIn(Object.values(JOB_TYPE)).withMessage("Invalid job type"),
-    ]
-)
+export const validateJobInput = withValidationErrors([
+  body("company").notEmpty().withMessage("Company is required"),
+  body("position").notEmpty().withMessage("Position is required"),
+  body("jobLocation").notEmpty().withMessage("Job location is required"),
+  body("jobStatus")
+    .isIn(Object.values(JOB_STATUS))
+    .withMessage("Invalid job status"),
+  body("jobType").isIn(Object.values(JOB_TYPE)).withMessage("Invalid job type"),
+]);
 
-export const validateIdParam = withValidationErrors(
-    [
-      param("id").isMongoId().withMessage("Invalid job id"),
-    ]
-)
+export const validateIdParam = withValidationErrors([
+  param("id").custom(async (value) => {
+    const isValidId = mongoose.Types.ObjectId.isValid(value);
+    if (!isValidId) {
+      throw new BadRequestError("Invalid MongoDB job id");
+    }
+    const job = await Job.findById(value);
+    if (!job) {
+      throw new NotFoundError(`No job with id ${value} was found`);
+    }
+  }),
+]);
