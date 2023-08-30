1dbd892157fce67da088e07b1a499a458acf4d55
fedilayoub <ayoubzahi2016@gmail.com>
2023-08-30 17:01:02 +0100

    sent the jwt token via http only cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ import { StatusCodes } from "http-status-codes";
 import { hashPassword } from "../utils/passwordUtils.js";
 import { UnauthenticatedError } from "../errors/customErrors.js";
 import { checkPassword } from "../utils/passwordUtils.js";
+import { createJWT } from "../utils/tokenUtils.js";
+
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
@@ -14,10 +16,17 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
-  const isValidUser = user && (await checkPassword(req.body.password, user.password));
+  const isValidUser =
+    user && (await checkPassword(req.body.password, user.password));
   if (!isValidUser) {
     throw new UnauthenticatedError("Invalid credentials");
   }
-  
-  res.status(StatusCodes.OK).json({ user });
+  const token = createJWT({ userId: user._id, role: user.role });
+  const oneDayInMillis = 1000 * 60 * 60 * 24;
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDayInMillis),
+    secure: process.env.NODE_ENV === "production",
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged in successfully" });
 };
