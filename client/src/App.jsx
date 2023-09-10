b83c50e4caab5be69c87f3644ca87c8a8feac2bc
fedilayoub <ayoubzahi2016@gmail.com>
2023-09-10 16:25:03 +0100

    added login functionnality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,8 @@ import {
   Admin,
 } from "./pages";
 import { action as registerAction } from "./pages/Register";
+import { action as loginAction } from "./pages/Login";
+
 
 export const checkDarkTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -39,6 +41,7 @@ const router = createBrowserRouter([
       {
         path: "login",
         element: <Login />,
+        action: loginAction
       },
       {
         path: "dashboard",
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, redirect, useNavigation, Form } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post("/auth/login", data);
+    toast.success("You are now logged in");
+    return redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+    const { msg } = error.response.data;
+    toast.error(msg);
+    return error;
+  }
+};
 
 const Login = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Login</h4>
         <FormRow
@@ -21,19 +40,19 @@ const Login = () => {
           placeholder="Password"
           defaultValue={"secret123"}
         />
-        <button type="submit" className="btn btn-block">
-          Submit
+        <button disabled={isSubmitting} type="submit" className="btn btn-block">
+          {isSubmitting ? "Submitting..." : "Submit"}
         </button>
-        <button type="button" className="btn btn-block">
+        <button  type="button" className="btn btn-block">
           Explore the app
         </button>
         <p>
           Don't have an account?{" "}
           <Link to="/register" className="member-link">
-            Login
+            Register
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
