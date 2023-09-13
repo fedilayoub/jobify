e1a75e0afc301f78e2337b5d170b86ce1351f9a2
fedilayoub <ayoubzahi2016@gmail.com>
2023-09-13 22:52:59 +0100

    added create job form initial changes

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { FormRow } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
 
 const AddJob = () => {
-  return <div>AddJob</div>;
+  const { user } = useOutletContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">Add job</h4>
+        <div className="form-center">
+          <FormRow type="text" name="position" />
+          <FormRow type="text" name="company" />
+          <FormRow
+            type="text"
+            name="jobLocation"
+            labelText="Job Location"
+            defaultValue={user?.location}
+          />
+          <button
+            disabled={isSubmitting}
+            type="submit"
+            className="btn btn-block form-btn"
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default AddJob;
