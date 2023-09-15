d6916a2687edfa0c729227d05583609e46ea5c6b
fedilayoub <ayoubzahi2016@gmail.com>
2023-09-15 16:35:46 +0100

    added Jobs container

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,7 +1,24 @@
+import Job from "./Job";
+import Wrapper from "../assets/wrappers/JobsContainer";
+import { useAllJobsContext } from "../pages/AllJobs";
+
 const JobsContainer = () => {
+  const {
+    data: { jobs },
+  } = useAllJobsContext();
+  if (jobs.length === 0)
     return (
-        <div>JobsContainer</div>
-    )
+      <Wrapper>
+        <h2>No jobs to display</h2>
+      </Wrapper>
+    );
+  return (
+    <Wrapper>
+      {jobs.map((job) => (
+        <Job key={job._id} job={job} />
+      ))}
+    </Wrapper>
+  );
 };
 
 export default JobsContainer;
