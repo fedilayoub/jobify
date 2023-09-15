1d7ef00464ba2c8a4f448efd664c71f33dfe2dd6
fedilayoub <ayoubzahi2016@gmail.com>
2023-09-15 11:29:43 +0100

    created all jobs context

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,12 +1,12 @@
-import { toast } from 'react-toastify';
-import { JobsContainer, SearchContainer } from '../components';
-import customFetch from '../utils/customFetch';
-import { useLoaderData } from 'react-router-dom';
-import { useContext, createContext } from 'react';
+import { toast } from "react-toastify";
+import { JobsContainer, SearchContainer } from "../components";
+import customFetch from "../utils/customFetch";
+import { useLoaderData } from "react-router-dom";
+import { useContext, createContext } from "react";
 
 export const loader = async ({ request }) => {
   try {
-    const { data } = await customFetch.get('/jobs');
+    const { data } = await customFetch.get("/jobs");
     return {
       data,
     };
@@ -15,16 +15,18 @@ export const loader = async ({ request }) => {
     return error;
   }
 };
-
+const AllJobsContext = createContext();
 const AllJobs = () => {
   const { data } = useLoaderData();
 
   return (
-    <>
+    <AllJobsContext.Provider value={{ data }}>
       <SearchContainer />
       <JobsContainer />
-    </>
+    </AllJobsContext.Provider>
   );
 };
+
+export const useAllJobsContext = () => useContext(AllJobsContext);
 
 export default AllJobs;
