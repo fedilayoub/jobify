de3b117e0537a7302dd3fdd0f092f4055593be67
fedilayoub <ayoubzahi2016@gmail.com>
2023-09-23 21:54:20 +0100

    added show stats controller

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -6,10 +6,12 @@ import {
   getJob,
   editJob,
   deleteJob,
+  showStats,
 } from "../controllers/jobController.js";
 import { validateJobInput,validateIdParam } from "../middleware/validationMiddleware.js";
 
 router.route("/").get(getAllJobs).post(validateJobInput, createJob);
+router.route('/stats',showStats)
 router
   .route("/:id")
   .get(validateIdParam,getJob)
