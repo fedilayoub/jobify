ffd64c84edc795f4366bdf755758a9acc62f29f0
fedilayoub <ayoubzahi2016@gmail.com>
2023-09-25 13:00:05 +0100

    added stats

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,7 @@ import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
 import { loader as adminLoader } from "./pages/Admin";
+import { loader as statsLoader } from "./pages/Stats";
 
 export const checkDarkTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -64,6 +65,7 @@ const router = createBrowserRouter([
           {
             path: "stats",
             element: <Stats />,
+            loader: statsLoader
           },
           {
             path: "all-jobs",
