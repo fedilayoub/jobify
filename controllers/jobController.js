9c3ecebee774c5beb955476333c993f8ba3baa1d
fedilayoub <ayoubzahi2016@gmail.com>
2023-09-26 22:44:14 +0100

    added pagination

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -29,9 +29,19 @@ export const getAllJobs = async (req, res) => {
     "z-a": "-position",
   };
 
+  //setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
   const sortKey = sortOptions[sort] || sortOptions.newest;
-  const jobs = await Job.find(queryObject).sort(sortKey);
-  res.status(StatusCodes.OK).json({ jobs });
+  const numOfPages = Math.ceil(totalJobs / limit);
+  const totalJobs = await Job.countDocuments(queryObject);
+  const jobs = await Job.find(queryObject)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 export const createJob = async (req, res) => {
