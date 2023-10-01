43955654afcc67d4d6d4b5c6c811d59e8fb93957
fedilayoub <ayoubzahi2016@gmail.com>
2023-10-01 11:39:28 +0100

    added global loading

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,7 +1,13 @@
 import React, { createContext, useContext, useState } from "react";
-import { Outlet, redirect, useLoaderData, useNavigate } from "react-router-dom";
+import {
+  Outlet,
+  redirect,
+  useLoaderData,
+  useNavigate,
+  useNavigation,
+} from "react-router-dom";
 import Wrapper from "../assets/wrappers/Dashboard";
-import { BigSidebar, Navbar, SmallSidebar } from "../components";
+import { BigSidebar, Navbar, SmallSidebar, Loading } from "../components";
 import { checkDarkTheme } from "../App";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
@@ -21,6 +27,8 @@ const DashboardLayout = () => {
   const navigate = useNavigate();
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(checkDarkTheme());
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
   const toggleDarkTheme = () => {
     const newIsDarkTheme = !isDarkTheme;
     setIsDarkTheme((prevState) => !prevState);
@@ -56,7 +64,7 @@ const DashboardLayout = () => {
           <div>
             <Navbar />
             <div className="dashboard-page">
-              <Outlet context={{ user }} />
+              {isPageLoading ? <Loading /> : <Outlet context={{ user }} />}
             </div>
           </div>
         </main>
