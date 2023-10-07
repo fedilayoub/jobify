b3e31a1899036e45e9ab8b449228d20fa686e52d
fedilayoub <ayoubzahi2016@gmail.com>
2023-10-07 11:51:40 +0100

    added rate limiter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,10 +2,16 @@ import Router from "express";
 const router = Router();
 import { register, login, logout } from "../controllers/authController.js";
 import { validateRegisterInput, validateLoginInput } from "../middleware/validationMiddleware.js";
+import rateLimiter from 'express-rate-limit';
 
 
-router.post("/login",validateLoginInput,login);
-router.post("/register",validateRegisterInput,register);
+const apiLimiter = rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 15,
+    message: { msg: 'IP rate limit exceeded, retry in 15 minutes.' },
+  });
+router.post("/login",apiLimiter,validateLoginInput,login);
+router.post("/register",apiLimiter,validateRegisterInput,register);
 router.get("/logout",logout)
 
 
